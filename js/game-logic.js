962ec365a448e8b512af00ea82293a60a81d2125
Jacob <jacob.seguino@gmail.com>
2017-12-15 00:57:12 -0600

    almost done, just need two edge cases

--- js/game-logic.js
+++ js/game-logic.js
@@ -3,27 +3,36 @@ let playerOneMoveOneType, playerOneMoveOneValue, playerOneMoveTwoType, playerOne
 
 let playerTwoMoveOneType, playerTwoMoveOneValue, playerTwoMoveTwoType, playerTwoMoveTwoValue, playerTwoMoveThreeType, playerTwoMoveThreeValue;
 
-const MaxPoints = 99;
+const maxPoints = 99;
 
 let playerOne = 0;
 let playerTwo = 0;
 
 const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
-    
-    validType(moveOneType);
-    validType(moveOneType);
-    validType(moveOneType);
-
-    checkValueOf(moveOneValue);
-    checkValueOf(moveTwoValue);
-    checkValueOf(moveThreeValue);
-
+    if(!validType(moveOneType)){
+        return null;
+    }
+    if(!validType(moveTwoType)){
+        return null;
+    }
+    if(!validType(moveThreeType)){
+        return null;
+    }
+    if(checkValueOf(moveOneValue)){
+        return null;
+    }
+    if(checkValueOf(moveTwoValue)){
+        return null;
+    }
+    if(checkValueOf(moveThreeValue)){
+        return null;
+    }
     if(moveOneValue === null || moveTwoValue === null || moveThreeValue === null){
         console.log("A value is missing from either Value One:"+moveOneValue+", Value Two:"+moveTwoValue+" or Value Three:"+moveThreeValue);
         return null;
     }
     if (moveOneValue + moveTwoValue + moveThreeValue > 99){
-        console.log("compaind value of " +player+" is more then 99");
+        console.log("the combined value of " +player+" is more then 99");
         return null;
     }
     if (player === "Player One"){
@@ -55,6 +64,11 @@ const getRoundWinner = (roundNumber) => {
           playerTwoMoveTwoType === null || playerTwoMoveTwoValue === null || playerTwoMoveThreeType === null || playerTwoMoveThreeValue === null){
         return null;
     }
+    // if (validType(playerOneMoveOneType) || !checkValueOf(playerOneMoveOneValue) || validType(playerOneMoveTwoType) || !checkValueOf(playerOneMoveTwoValue) ||
+    // validType(playerOneMoveThreeType) || !checkValueOf(playerOneMoveThreeValue) || validType(playerTwoMoveOneType)  || !checkValueOf(playerTwoMoveOneValue) ||
+    // validType(playerTwoMoveTwoType) || !checkValueOf(playerTwoMoveTwoValue) || validType(playerTwoMoveThreeType)  || !checkValueOf(playerTwoMoveThreeValue)){
+    //     return null;
+    // }
    let playerOneType, playerOneValue, playerTwoType, playerTwoValue;
    switch (roundNumber) {
         case 1:
@@ -84,53 +98,116 @@ const getRoundWinner = (roundNumber) => {
        if(playerOneValue === playerTwoValue){
         return 'Tie';
        }else if (playerOneValue > playerTwoValue){
-           return 'Player One';
+        playerOne++;
+        return 'Player One';
        }else{
-           return 'Player Two';
+        playerTwo++;
+        return 'Player Two';
        } 
    }
    if (playerOneType === 'rock'){
        if (playerTwoType === 'scissors'){
-           return 'Player One';
+        playerOne++;
+        return 'Player One';
        }else{
-           return 'Player Two';
+        playerTwo++;
+        return 'Player Two';
        }
    }
    if (playerOneType === 'scissors'){
        if (playerTwoType === 'paper'){
-           return 'Player One';
+        playerOne++;
+        return 'Player One';
        }else{
-           return 'Player Two';
+        playerTwo++;
+        return 'Player Two';
        }
    }
    if (playerOneType === 'paper'){
       if (playerTwoType === 'rock'){
-          return 'Player One';
+        playerOne++;
+        return 'Player One';
       }else{
-          return 'Player Two';
+        playerTwo++;
+        return 'Player Two';
       }
    }
 
 }
 
 const getGameWinner = () => {
-
-
+    playerOne = 0;
+    playerTwo = 0;
+    getRoundWinner(1);
+    getRoundWinner(2);
+    getRoundWinner(3);
+    if(playerOne > playerTwo){
+        console.log("Player One wins");
+        return 'Player One';
+    }else if(playerOne < playerTwo){
+        console.log("Player Two wins");
+        return 'Player Two';
+    }else{
+        console.log("Tie");
+        return 'Tie';
+    }
 }
 
 const setComputerMoves = () => {
+    playerTwoMoveOneType = computerType();
+    playerTwoMoveTwoType = computerType();
+    playerTwoMoveThreeType = computerType();
 
+    computerValueOne = Math.floor(Math.random() * (maxPoints-2));
+    computerValueTwo = Math.floor(Math.random() * (maxPoints - computerValueOne));
+    computerValueThree = (maxPoints - (computerValueOne + computerValueTwo));
+
+    if (computerValueOne + computerValueTwo + computerValueThree > 99){
+        console.log("The combined value of computer's choices is more then 99");
+        return null;
+    }
+
+    playerTwoMoveOneValue = computerValueOne;
+    console.log("Computer picked the value one to be: "+playerTwoMoveOneValue);
+    playerTwoMoveTwoValue = computerValueTwo;
+    console.log("Computer picked the value two to be: "+playerTwoMoveTwoValue);
+    playerTwoMoveThreeValue = computerValueThree;
+    console.log("Computer picked the value three to be: "+playerTwoMoveThreeValue);
 }
 
-const checkValueOf = (Value) => {
-    if(value > 99 || value <= 0){
+const checkValueOf = (value) => {
+    if(value > 99 || value <= 0 || !Number.isInteger(value)){
         console.log("value one is set at " +value+ " which is out side the bounds");
-        return null
+        return true
     }
+    return false
 }
 const validType = (type) => {
-    if(type !== 'rock' || type !== 'paper' || type !== 'scissors'){
-        console.log("Choice one of "+type+" does not match the choices 'rock', 'paper', or 'scissors");
-        return null;
+    if(type === 'rock' || type === 'paper' || type === 'scissors'){
+        return true;
+    }
+    console.log("Choice of "+type+" does not match the choices 'rock', 'paper', or 'scissors");
+    return false;
+}
+
+const computerType = () => {
+    type = Math.floor(Math.random() * 3);
+//    console.log("Random number was: "+type);
+    switch (type) {
+        case 0:
+        console.log("computer picked rock");
+        return 'rock';
+        break;
+        case 1:
+        console.log("computer picked scissors");
+        return 'scissors';
+        break;
+        case 2:
+        console.log("computer picked paper");
+        return 'paper';
+        break;
+        default:
+        console.log("nothing was picked");
+        break;
     }
 }
